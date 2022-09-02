62eaa0ea811caed8949a7952934ddd133c577d98
gaga <jcky0511@gmail.com>
2022-09-02 12:50:40 +0900

    배경

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div>
+        <div class="nav">
+          <div class="memu">
+            <img src="img/메뉴.png" alt="메뉴"/> {/*메뉴 이미지*/}
+            <img src="img/로그인.png" alt="메뉴"/> {/*메뉴 이미지*/}
+            <img src="img/로고.png" alt="메뉴"/> {/*메뉴 이미지*/}
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./main.css";
+import Nav from "./Nav";
+
+
+
+function main() {
+  return (
+    <div>
+      <img src="img/배경.png" />
+      <Nav />
+    </div>
+  )
+}
+
+export default main
